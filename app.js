21a7f26dfa2bbc73e0a06de95a87c084078e7454
Omar Mohamed Yousef <mourra24@gmail.com>
2024-05-16 16:22:05 +0300

    fix

--- app.js
+++ app.js
@@ -5,112 +5,129 @@ const { Server } = require("socket.io");
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {});
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
-const MongoClient = require('mongodb').MongoClient
-const uri = "mongodb+srv://omarelbanna:AtieyhIE9Xj2wCKk@security.yxz4gwr.mongodb.net/?retryWrites=true&w=majority&appName=security";
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://omarelbanna:AtieyhIE9Xj2wCKk@security.yxz4gwr.mongodb.net/?retryWrites=true&w=majority&appName=security";
 const client = new MongoClient(uri);
 let db;
 let users;
 let keys;
 async function connect() {
-    try {
-      await client.connect();
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        db = client.db("Security");
-        users = db.collection("Users");
-        keys = db.collection("Keys"); 
-    }
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    db = client.db("Security");
+    users = db.collection("Users");
+    keys = db.collection("Keys");
   }
+}
 connect().catch(console.dir);
 
-const createUser = async (username, hashedPassword,publicKey) =>{
-    try{
-        const user = await users.findOne({"username":username});
-        if(user){
-            return {success:false, message:"User exists"};
-        }
-        await users.insertOne({"username":username,"password":hashedPassword});
-        await keys.insertOne({"username":username,"key":publicKey});
-        return{success : true, message:'User has been created successfully'};
+const createUser = async (username, hashedPassword, publicKey) => {
+  try {
+    const user = await users.findOne({ username: username });
+    if (user) {
+      return { success: false, message: "User exists" };
     }
-    catch (error) {
-        console.error('Error during signup:', error);
-        return { success: false, message: 'server error' };
-      }
-
+    await users.insertOne({ username: username, password: hashedPassword });
+    await keys.insertOne({ username: username, key: publicKey });
+    return { success: true, message: "User has been created successfully" };
+  } catch (error) {
+    console.error("Error during signup:", error);
+    return { success: false, message: "server error" };
+  }
 };
 
 const authenticateUser = async (username, hashedPassword) => {
-    try {
-      const user = await users.findOne({ "username":username });
-      if (!user) {
-        return { success: false, message: 'User not found.' };
-      }
-      console.log(user.password);
-      console.log(hashedPassword);
-      const passwordMatch = user.password === hashedPassword ;
-      console.log(passwordMatch);
-      if (!passwordMatch) {
-        return { success: false, message: 'Invalid password.' };
-      }
-  
-      return { success: true, user };
-    } catch (error) {
-      console.error('Error during authentication:', error);
-      return { success: false, message: 'server error' };
+  try {
+    const user = await users.findOne({ username: username });
+    if (!user) {
+      return { success: false, message: "User not found." };
     }
-  };
+    console.log(user.password);
+    console.log(hashedPassword);
+    const passwordMatch = user.password === hashedPassword;
+    console.log(passwordMatch);
+    if (!passwordMatch) {
+      return { success: false, message: "Invalid password." };
+    }
 
-  io.on('connection', (socket) => {
-    console.log('A user connected');
-  
-    socket.on('login', async ({ username, hashedPassword }) => {
-        const { success, message, user } = await authenticateUser(username, hashedPassword);
-        if (success) {
-          socket.user = user;
-          socket.emit('loginResponse', { success: true, message: 'Login successful', user });
-        } else {
-          socket.emit('loginResponse', { success: false, message });
-        }
+    return { success: true, user };
+  } catch (error) {
+    console.error("Error during authentication:", error);
+    return { success: false, message: "server error" };
+  }
+};
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  // socket.emit("readMessage_test", "hamada");
+  socket.on("login", async ({ username, hashedPassword }) => {
+    const { success, message, user } = await authenticateUser(
+      username,
+      hashedPassword
+    );
+    if (success) {
+      socket.user = user;
+      socket.emit("loginResponse", {
+        success: true,
+        message: "Login successful",
+        user,
       });
-      
-
-    socket.on('signup',async({username, hashedPassword,publicKey})=>{
-        const {success, message} = await createUser(username,hashedPassword,publicKey);
-
-        if(success){
-            socket.emit('signupResponse', { success: true, message: 'Signup successful'});
-        }
-        else{
-            socket.emit('signupResponse', { success: false, message});
-        }
-    });
-    
-
-    socket.on("getAllUsers",async(username)=>{
-        try{
-            const response = await keys.find({"username":{$ne:username}}).project({ username: 1, key: 1 }).toArray();
-            socket.emit("getAllUsersResponse",{"users":response});
-        }
-        catch(error){
-            console.log(error);
-        }
-    });
-
-    socket.on("message",async({cyphertext, cypherkey, user})=>{
-      const sender = socket.user.username;
-      console.log(sender);
-      socket.emit("readMessage_"+String(user),{cyphertext,cypherkey,sender});
+    } else {
+      socket.emit("loginResponse", { success: false, message });
+    }
   });
 
+  socket.on("signup", async ({ username, hashedPassword, publicKey }) => {
+    const { success, message } = await createUser(
+      username,
+      hashedPassword,
+      publicKey
+    );
 
-
+    if (success) {
+      socket.emit("signupResponse", {
+        success: true,
+        message: "Signup successful",
+      });
+    } else {
+      socket.emit("signupResponse", { success: false, message });
+    }
   });
+
+  socket.on("getAllUsers", async (username) => {
+    try {
+      const response = await keys
+        .find({ username: { $ne: username } })
+        .project({ username: 1, key: 1 })
+        .toArray();
+      socket.emit("getAllUsersResponse", { users: response });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  socket.on("message", async ({ cyphertext, cypherkey, user }) => {
+    const sender = socket.user.username;
+    console.log(sender);
+    const channel = "readMessage_" + String(user);
+
+    io.emit(channel, {
+      cyphertext,
+      cypherkey,
+      sender,
+    });
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 httpServer.listen(PORT, () => {
-console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
